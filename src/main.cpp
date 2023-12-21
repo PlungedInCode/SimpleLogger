859bb2f3746377d0af1232a0682820d3925248da
PlungedInCode <plungedincode@gmail.com>
2023-12-21 22:03:41 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,11 @@
+#include <fstream>
 #include <iostream>
-
 
 #include "Logger.hpp"
 #include "test.hpp"
 
-int main() {
-  // auto& logger = Logger::GetInstance();
-  // logger.Warning("1", 2, "3", "4", "5", 6, "7");
-  // Logger::SetLogLevel(LogLevel::kWarning);
-  // Logger::SetLogFile("log11.txt");
-  // Logger::SetStream(OutputStream::kBoth);
-  // Logger::Error("Hello", ",", "World", "!", "2", "*", 2.4, "=", 4.8);
-  // test(1);
-  // Logger::SetLogLevel(LogLevel::kTrace);
-  // test(2);
-
-  threadTest();
+void PrinterCH(std::ostream& out, std::string text) {
+  out << text << std::endl;
 }
+
+int main() { runTest(); }
