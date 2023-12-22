14c8f98830b121f6ba674702013fa0f6750ee958
PlungedInCode <plungedincode@gmail.com>
2023-12-22 11:29:13 +0300

    File/Line stamps

--- include/Logger.hpp
+++ include/Logger.hpp
@@ -1,16 +1,17 @@
 #ifndef _LOGGER_HPP_
 #define _LOGGER_HPP_
 
-
-#include <iostream>
+#include <chrono>
 #include <fstream>
 #include <iomanip>
-#include <vector>
-#include <chrono>
+#include <iostream>
 #include <mutex>
+#include <vector>
 
 #define RESET_COLOR "\033[0m"
 #define NO_COLOR ""
+#define IN_FILE "in file "
+#define ON_LINE ":"
 
 enum LogLevel {
   kTrace = 0,
@@ -24,7 +25,7 @@ const std::vector<std::string> LogLevelNames = {
     "[TRACE]   ", "[DEBUG]   ", "[INFO]    ", "[WARNING] ", "[ERROR]   "};
 
 const std::vector<std::string> LogLevelColors = {
-    "\033[1;33m",        // Yellow
+    "\033[1;35m",        // Purple
     "\033[1;34m",        // Blue
     "\033[1;32m",        // Green
     "\033[1;38;5;208m",  // Orange
@@ -33,8 +34,8 @@ const std::vector<std::string> LogLevelColors = {
 };
 
 enum OutputStream { kConsole, kFile, kBoth };
-const std::vector<std::string> OutputStreamNames = {"CONSOLE", "FILE",
-                                                    "CONSOLE/FILE"}; // I don't think that i need this...
+const std::vector<std::string> OutputStreamNames = {
+    "CONSOLE", "FILE", "CONSOLE/FILE"};  // I don't think that i need this...
 
 class Logger {
  public:
@@ -48,35 +49,47 @@ class Logger {
   static void SetLogFile(const std::string& log_filename);
   static void EnableTimeStamp();
   static void DisableTimeStamp();
+  static void EnableFileStamp();
+  static void DisableFileStamp();
+
 
   template <typename... Args>
-  static void Trace(const Args&... args);
+  static void Trace(int line_number, const std::string& file_name, const Args&... args);
 
   template <typename... Args>
-  static void Debug(const Args&... args);
+  static void Debug(int line_number, const std::string& file_name, const Args&... args);
 
   template <typename... Args>
-  static void Info(const Args&... args);
+  static void Info(int line_number, const std::string& file_name, const Args&... args);
 
   template <typename... Args>
-  static void Warning(const Args&... args);
+  static void Warning(int line_number, const std::string& file_name, const Args&... args);
 
   template <typename... Args>
-  static void Error(const Args&... args);
+  static void Error(int line_number, const std::string& file_name, const Args&... args);
 
  private:
   Logger();
 
-  static LogLevel log_level_;
-  static OutputStream log_output_;
-  static std::string log_filename_;
-  static std::mutex log_mutex_;
-  static std::ofstream log_foutput_;
-  static bool time_stamp_;
+  LogLevel log_level_;
+  OutputStream log_output_;
+  std::string log_filename_;
+  std::mutex log_mutex_;
+  std::ofstream log_foutput_;
+  bool time_stamp_;
+  bool file_stamp_;
 
   template <typename... Args>
-  static void Log(LogLevel log_level, const Args&... args);
+  void Log(LogLevel log_level, const Args&... args);
 };
+
+
+#define LOG_TRACE(...)    (Logger::Trace(__LINE__, __FILE__, __VA_ARGS__))
+#define LOG_INFO(...)     (Logger::Info(__LINE__, __FILE__, __VA_ARGS__))
+#define LOG_DEBUG(...)    (Logger::Debug(__LINE__, __FILE__, __VA_ARGS__))
+#define LOG_WARNING(...)  (Logger::Warning(__LINE__, __FILE__, __VA_ARGS__))
+#define LOG_ERROR(...)    (Logger::Error(__LINE__, __FILE__, __VA_ARGS__))
+
 
 #include "../src/Logger.tpp"  // Include the template implementation
 #endif
